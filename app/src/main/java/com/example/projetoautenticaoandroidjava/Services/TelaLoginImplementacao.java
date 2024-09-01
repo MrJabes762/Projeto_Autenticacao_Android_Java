e5064ea0b89aa63bf988ac7a59a557fad0fac86b
Mr Jabes <jabessilva31@gmail.com>
2024-09-01 19:33:37 -0300

    Atuaizações: Construção inicial da tela Login parte logica

---
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaLoginImplementacao.java
@@ -0,0 +1,15 @@
+package com.example.projetoautenticaoandroidjava.Services;
+
+import com.example.projetoautenticaoandroidjava.Controllers.TelaLogin;
+
+public class TelaLoginImplementacao extends TelaLogin {
+    @Override
+    public void inicializarComponentes() {
+
+    }
+
+    @Override
+    public void Observadores() {
+
+    }
+}
