7c0ece7956e1f16618e0a11b6325c7588cbbf58d
Mr Jabes <jabessilva31@gmail.com>
2024-09-02 17:37:13 -0300

    Atualizações:
    - Tela Principal e Tela Prncipal Implementação

--- app/src/main/java/com/example/projetoautenticaoandroidjava/Controllers/TelaPrincipal.java
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Controllers/TelaPrincipal.java
@@ -1,19 +1,52 @@
 package com.example.projetoautenticaoandroidjava.Controllers;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.example.projetoautenticaoandroidjava.R;
 import com.example.projetoautenticaoandroidjava.Services.Operacoes;
 
 public abstract class TelaPrincipal extends AppCompatActivity implements Operacoes {
+    // Objetos
+    // TextsViews
+    private TextView textExibirUsuairo;
+    private TextView textExibirEmail;
+    // Botoao
+    private AppCompatButton btnDeslogar;
 
+    //Metoodo Princpla de Criação da Tela
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
         inicializarComponentes();
         Observadores();
+    }
+
+    protected TextView getTextExibirUsuairo() {
+        return textExibirUsuairo;
+    }
+
+    protected void setTextExibirUsuairo(TextView textExibirUsuairo) {
+        this.textExibirUsuairo = textExibirUsuairo;
+    }
+
+    protected TextView getTextExibirEmail() {
+        return textExibirEmail;
+    }
+
+    protected void setTextExibirEmail(TextView textExibirEmail) {
+        this.textExibirEmail = textExibirEmail;
+    }
+
+    protected AppCompatButton getBtnDeslogar() {
+        return btnDeslogar;
+    }
+
+    protected void setBtnDeslogar(AppCompatButton btnDeslogar) {
+        this.btnDeslogar = btnDeslogar;
     }
 }
--- app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaPrincipal/TelaPrincipalImplementacao.java
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaPrincipal/TelaPrincipalImplementacao.java
@@ -1,15 +1,31 @@
 package com.example.projetoautenticaoandroidjava.Services.TelaPrincipal;
 
 import com.example.projetoautenticaoandroidjava.Controllers.TelaPrincipal;
+import com.example.projetoautenticaoandroidjava.R;
+import com.example.projetoautenticaoandroidjava.Services.Direcionador.DirecionadorDeLayout;
+import com.example.projetoautenticaoandroidjava.Services.TelaLogin.TelaLoginImplementacao;
 
 public class TelaPrincipalImplementacao extends TelaPrincipal {
     @Override
     public void inicializarComponentes() {
+        setEditTexs();
+        setBtnDeslogar(findViewById(R.id.btnDeslogar));
+    }
 
+    private void setEditTexs() {
+        setTextExibirEmail(findViewById(R.id.textExibirEmail));
+        setTextExibirUsuairo(findViewById(R.id.textexibirUsuario));
     }
 
     @Override
     public void Observadores() {
+        getBtnDeslogar().setOnClickListener(v -> {
+            voltarParaLogin();
+        });
+    }
 
+    private void voltarParaLogin() {
+        DirecionadorDeLayout.irParaLayout(this, TelaLoginImplementacao.class);
+        finish();
     }
 }
