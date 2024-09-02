5fd1d7d3da987dd11d26b2c73b5e9e9da4cce910
Jabes da Silva Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2024-09-02 17:40:03 -0300

    Desenvolvimento: Mapeamento dos Objetos e  Logicas iniciais de alguns Botões  (#7)
    * Atuaizações: algumns desenhaveis de fundo da aplicação
    
    * Atuaizações: Construção inicial da tela Login parte visual
    
    * Atuaizações: Construção inicial da tela Login parte logica
    
    * Atuaizações: Interface Base de métodos principais para serem aplicadas na aplicação
    
    * Atuaizações: Ajuste de Repositorio
    
    * Atuaizações: criação de un icone para o login
    
    * Atuaizações: ajuste de strings.xml
    
    * Atuaizações: Fundo de Botões
    
    * Atuaizações: Ajuste Btn
    
    * Atualizações: Fundo da Edit Text
    
    * Atualizações: Tela Login Parte Visual Pronta
    
    * Atualizações: icones de email e senha
    
    * Atualizações: Direcionador de Layouts
    
    * Atualizações: Tela Login parte visual finalizada
    
    * Atualizações: Ajustes de repositorio
    
    * Atualizações: Construção Inicial Da tela form Cadastro
    
    * Atualizações: Construção Inicial Da tela form Cadastro parte Logica
    
    * Atualizações: Finalizando parte visual form cadastro
    
    * Atualizações: Construção inicial tela principal parte visual
    
    * Atualizações: Construção inicial tela principal parte logica
    
    * Atualizações: Ajuste visual da tela form
    
    * Atualizações: Ajuste visual tela login
    
    * Atualizações: Ajuste visual tela login..
    
    * Atualizações: Ajuste visual tela form cadastro..
    
    * Atualizações: Adiçao de String
    
    * Atualizações: Ajuste final Tela login
    
    * Atualizações: Finalzando Tela Principal Parte visual
    
    * Atualizações: ajustes de orientação das telas
    
    * Atualizações: Configurando o Banco de Dados - Firebase
    
    * Atualizações:
     * Adicionando as dependencias responsaveis pela funcionalidade do banco de dados
     * Ajuste de repositorio de dependencias
    
    * Atualizações:
    - Tela Login e Tela Login Implementação
     * Configurando os IDS dos Objetos da tela
     * Utilizando artificio de armazenamento em HeshMap para acesso Rápido
    
    * Atualizações:
    - Tela Login e Tela Login Implementação
     * Configuração Inicial da Progress Bar
    
    * Atualizações:
    - Tela Form Cadastro e Tela Form Cadastro Implementação:
    
     * Set dos Objetos na tela
    
    * Atualizações:
    - Tela Form Cadastro Implementação:
    
     * Logica Inicia do botão
    
    * Atualizações:
    
    - Tela Principal e Tela Prncipal Implementação

--- app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaFormCadastro/TelaFormCadastroImplementacao.java
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaFormCadastro/TelaFormCadastroImplementacao.java
@@ -1,15 +1,32 @@
 package com.example.projetoautenticaoandroidjava.Services.TelaFormCadastro;
 
 import com.example.projetoautenticaoandroidjava.Controllers.TelaFormCadastro;
+import com.example.projetoautenticaoandroidjava.R;
+import com.example.projetoautenticaoandroidjava.Services.Direcionador.DirecionadorDeLayout;
+import com.example.projetoautenticaoandroidjava.Services.TelaLogin.TelaLoginImplementacao;
 
 public class TelaFormCadastroImplementacao extends TelaFormCadastro {
     @Override
     public void inicializarComponentes() {
+        setEditTexts();
+        setBtnCadastrarForm(findViewById(R.id.btnCadstrarForm));
+    }
 
+    private void setEditTexts() {
+        setPegarNomeCadastro(findViewById(R.id.pegarNomeCadastro));
+        setPegarEmailCadastro(findViewById(R.id.pegarEmailCadastro));
+        setPegarSenhaCadastro(findViewById(R.id.pegarSenhaCadastro));
     }
 
     @Override
     public void Observadores() {
+        getBtnCadastrarForm().setOnClickListener(v -> {
+            voltarParaTelaLogin();
+        });
+    }
 
+    private void voltarParaTelaLogin() {
+        DirecionadorDeLayout.irParaLayout(this, TelaLoginImplementacao.class);
+        finish();
     }
 }
--- app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaLogin/TelaLoginImplementacao.java
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaLogin/TelaLoginImplementacao.java
@@ -1,15 +1,74 @@
 package com.example.projetoautenticaoandroidjava.Services.TelaLogin;
 
+import android.view.View;
+import android.widget.ProgressBar;
+
 import com.example.projetoautenticaoandroidjava.Controllers.TelaLogin;
+import com.example.projetoautenticaoandroidjava.R;
+import com.example.projetoautenticaoandroidjava.Services.Direcionador.DirecionadorDeLayout;
+import com.example.projetoautenticaoandroidjava.Services.TelaFormCadastro.TelaFormCadastroImplementacao;
+import com.example.projetoautenticaoandroidjava.Services.TelaPrincipal.TelaPrincipalImplementacao;
+
+import java.util.HashMap;
 
 public class TelaLoginImplementacao extends TelaLogin {
     @Override
     public void inicializarComponentes() {
+        setHashMapsECaches();
+        setEditTexts();
+        setBtns();
+        setProgressBar(findViewById(R.id.Progressbar));
+    }
 
+    private void setBtns() {
+        setBtnEntrar(findViewById(R.id.btnEntrar));
+        setBtnCadastrar(findViewById(R.id.btnCadastrar));
+    }
+
+    private void setEditTexts() {
+        setPegarEmail(findViewById(R.id.pegarEmail));
+        setPegarSenha(findViewById(R.id.pegarSenha));
+    }
+
+    private void setHashMapsECaches() {
+        setLayoutCache(new HashMap<>());
+        setCachedLayoutPrincipal(null);
+        setCachedLayoutFormCadastro(null);
+        setLayoutClass(null);
+    }
+    private int alterarVisibilidade (ProgressBar progress){
+        return progress.getVisibility() == View.VISIBLE ? View.GONE: View.VISIBLE;
     }
 
     @Override
     public void Observadores() {
+        getBtnEntrar().setOnClickListener(v -> {
+            irTelaPrincipal();
+        });
+        getBtnCadastrar().setOnClickListener(v -> {
+            irTelaFormCadastro();
+        });
+    }
 
+    private void irTelaFormCadastro() { // verificação se o Hash Map esta vazio se não ja inicializa
+        if (getCachedLayoutFormCadastro()!= null) {
+            DirecionadorDeLayout.irParaLayout(this, getCachedLayoutFormCadastro());
+        } else {
+            setLayoutClass(TelaFormCadastroImplementacao.class);
+            DirecionadorDeLayout.irParaLayout(this, getLayoutClass());
+            getLayoutCache().put(R.layout.activity_tela_form_cadastro, getLayoutClass());// Armazena o layout em cache
+        }
+        finish();
+    }
+
+    private void irTelaPrincipal() {
+        if (getCachedLayoutPrincipal() != null){
+            DirecionadorDeLayout.irParaLayout(this, getCachedLayoutPrincipal());
+        } else {
+            setLayoutClass(TelaPrincipalImplementacao.class);
+            DirecionadorDeLayout.irParaLayout(this, getLayoutClass());
+            getLayoutCache().put(R.layout.activity_tela_principal,getLayoutClass());// Armazena o layout em cache
+        }
+        finish();
     }
 }
