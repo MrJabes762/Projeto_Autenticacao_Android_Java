865b96024e0ba3e5f9d6e2f31a620b58995a9f63
Mr Jabes <jabessilva31@gmail.com>
2024-09-02 17:18:20 -0300

    Atualizações: - Tela Form Cadastro e Tela Form Cadastro Implementação:
     * Set dos Objetos na tela

--- app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaFormCadastro/TelaFormCadastroImplementacao.java
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaFormCadastro/TelaFormCadastroImplementacao.java
@@ -1,11 +1,19 @@
 package com.example.projetoautenticaoandroidjava.Services.TelaFormCadastro;
 
 import com.example.projetoautenticaoandroidjava.Controllers.TelaFormCadastro;
+import com.example.projetoautenticaoandroidjava.R;
 
 public class TelaFormCadastroImplementacao extends TelaFormCadastro {
     @Override
     public void inicializarComponentes() {
+        setEditTexts();
+        setBtnCadastrarForm(findViewById(R.id.btnCadstrarForm));
+    }
 
+    private void setEditTexts() {
+        setPegarNomeCadastro(findViewById(R.id.pegarNomeCadastro));
+        setPegarEmailCadastro(findViewById(R.id.pegarEmailCadastro));
+        setPegarSenhaCadastro(findViewById(R.id.pegarSenhaCadastro));
     }
 
     @Override
