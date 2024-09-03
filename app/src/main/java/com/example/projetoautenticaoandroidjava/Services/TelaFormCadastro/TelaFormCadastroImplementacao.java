7b4c9536bf111ec24c1ba58daaf4cc148c2c92dc
Mr Jabes <jabessilva31@gmail.com>
2024-09-03 01:45:13 -0300

    Atualizações:
    - Tela Form Cadastro Implementação
    
    * Implementação inicial do cadastro de usuarios com tratamento de exceção para campos nulos.

--- app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaFormCadastro/TelaFormCadastroImplementacao.java
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaFormCadastro/TelaFormCadastroImplementacao.java
@@ -1,11 +1,17 @@
 package com.example.projetoautenticaoandroidjava.Services.TelaFormCadastro;
 
+import android.widget.Toast;
+
 import com.example.projetoautenticaoandroidjava.Controllers.TelaFormCadastro;
+import com.example.projetoautenticaoandroidjava.Data.FirebaseFirestoreRepository;
 import com.example.projetoautenticaoandroidjava.R;
 import com.example.projetoautenticaoandroidjava.Services.Direcionador.DirecionadorDeLayout;
 import com.example.projetoautenticaoandroidjava.Services.TelaLogin.TelaLoginImplementacao;
+import com.example.projetoautenticaoandroidjava.model.Usuario;
 
 public class TelaFormCadastroImplementacao extends TelaFormCadastro {
+   private Usuario user;
+
     @Override
     public void inicializarComponentes() {
         setEditTexts();
@@ -21,12 +27,38 @@ public class TelaFormCadastroImplementacao extends TelaFormCadastro {
     @Override
     public void Observadores() {
         getBtnCadastrarForm().setOnClickListener(v -> {
-            voltarParaTelaLogin();
+            try {
+                vetificarESalvarDados();
+                voltarParaTelaLogin();
+            } catch (FormCadastroException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         });
     }
+
+    private void vetificarESalvarDados() throws FormCadastroException {
+        setUser(new Usuario(
+                getPegarNomeCadastro().getText().toString(),
+                getPegarEmailCadastro().getText().toString(),
+                getPegarSenhaCadastro().getText().toString()));
+        if (getUser().getNome().isEmpty() || getUser().getEmail().isEmpty() || getUser().getSenha().isEmpty()) {
+            throw new FormCadastroException("Os campos estão vazios. Preencha novamente.");
+        }else {
+            FirebaseFirestoreRepository.adicionarUsuario(this,getUser().getEmail(),getUser().getSenha());
+        }
+    }
+
 
     private void voltarParaTelaLogin() {
         DirecionadorDeLayout.irParaLayout(this, TelaLoginImplementacao.class);
         finish();
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
     }
 }
