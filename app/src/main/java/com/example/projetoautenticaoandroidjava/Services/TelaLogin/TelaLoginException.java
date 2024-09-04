bd381ad825b8d042ab44039e87ccf385111469e9
Mr Jabes <jabessilva31@gmail.com>
2024-09-04 19:08:09 -0300

    Atualizações:
    - Tela Login:
    
    * add nova exceção

---
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaLogin/TelaLoginException.java
@@ -0,0 +1,7 @@
+package com.example.projetoautenticaoandroidjava.Services.TelaLogin;
+
+public class TelaLoginException extends Exception{
+    public TelaLoginException(String mensagem) {
+        super(mensagem);
+    }
+}
