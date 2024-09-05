828065083b7d6e67f0b47bfc2e066c511b3e272c
Mr Jabes <jabessilva31@gmail.com>
2024-09-05 02:29:23 -0300

    Atualizações:
    - Tela Principal Implementação:
    
    * Add Metodo de Ciclo de vida do App pra pegar o Id do usuario em contexto no banco e pegar os dados
    
    * Criação de uma interface para pegar o nome ou um erroo

--- app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaPrincipal/TelaPrincipalImplementacao.java
+++ app/src/main/java/com/example/projetoautenticaoandroidjava/Services/TelaPrincipal/TelaPrincipalImplementacao.java
@@ -1,12 +1,16 @@
 package com.example.projetoautenticaoandroidjava.Services.TelaPrincipal;
 
+import android.widget.Toast;
+
 import com.example.projetoautenticaoandroidjava.Controllers.TelaPrincipal;
 import com.example.projetoautenticaoandroidjava.Data.FirebaseFirestoreRepository;
 import com.example.projetoautenticaoandroidjava.R;
 import com.example.projetoautenticaoandroidjava.Services.Direcionador.DirecionadorDeLayout;
 import com.example.projetoautenticaoandroidjava.Services.TelaLogin.TelaLoginImplementacao;
+import com.example.projetoautenticaoandroidjava.model.Usuario;
 
 public class TelaPrincipalImplementacao extends TelaPrincipal {
+    private String email;
     @Override
     public void inicializarComponentes() {
         setEditTexs();
@@ -30,8 +34,44 @@ public class TelaPrincipalImplementacao extends TelaPrincipal {
         FirebaseFirestoreRepository.delogarUsuario();
     }
 
+    protected void onStart() {
+        super.onStart();
+        String usuarioId = FirebaseFirestoreRepository.pegarUsuarioAtual().getUid();
+            Usuario.setId(usuarioId);
+            setEmail(FirebaseFirestoreRepository.pegarUsuarioAtual().getEmail());
+            FirebaseFirestoreRepository.pegarDados(Usuario.getId(), new Callback<String>() {
+                @Override
+                public void onSuccess(String nome) {// Implementação dos metodos
+                    if (nome != null && !nome.isEmpty()) {
+                        getTextExibirUsuairo().setText(nome);
+                    } else {
+                        Toast.makeText(TelaPrincipalImplementacao.this, "Usuario Não Encontrado", Toast.LENGTH_SHORT).show();
+                    }
+                    getTextExibirEmail().setText(getEmail());
+                }
+
+                @Override
+                public void onError(String error) {// em caso de erro
+                    Toast.makeText(getApplicationContext(), "Erro ao buscar dados: " + error, Toast.LENGTH_LONG).show();
+                }
+            });
+    }
+
+
     private void voltarParaLogin() {
         DirecionadorDeLayout.irParaLayout(this, TelaLoginImplementacao.class);
         finish();
+    }
+    public interface Callback<T> {// Interface para pegar o nme ou os dados
+        void onSuccess(T nome);
+        void onError(String error);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
